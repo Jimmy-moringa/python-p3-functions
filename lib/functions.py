975040c2e5cad1cdeb834e045cd71b32e97ea62b
jimmy-moringa <jimmy.musyoki@student.moringaschool.com>
2024-09-10 16:44:43 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,45 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, Programmer!")
+
+greet_programmer()
+
+
+
+
 
 def greet(name):
-    pass
+    print(f"Hello, " + name)
+
+
+greet("Naureen")
+
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}")
+
+greet_with_default()
+greet_with_default("jimmy")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print(add(57, 65))
 
+# Define the function
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number / 2
+
+
+print(halve(46))  
+
+
+print(halve("two"))  
+
+
+
+
